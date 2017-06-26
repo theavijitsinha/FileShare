4719a2bef84fa89134905fc517267113f8f6098f
Avijit Sinha <theavijitsinha@gmail.com>
2017-06-26 10:46:04 +0530

    Removed some unnecessary code
    Unnecessary code which was being used for debugging purposes

--- renderer.js
+++ renderer.js
@@ -2,28 +2,19 @@ const fileWatcher = require('./service/file-watcher.js')
 const fileServer = require('./service/file-server.js')
 const eventHandler = require('./service/event-handler.js')
 const peerDiscovery = require('./service/peer-discovery.js')
-const PeerConnection = require('./service/peer-connection.js')
 const Constants = require('./service/constants.js')
 
 eventHandler.listen(Constants.EVENT_FILE_SERVER_STARTED, () => {
   peerDiscovery.startDiscoverer()
 })
 
-eventHandler.listen(Constants.EVENT_PEER_UP, (serverNode) => {
-  console.log(`Found Node ${serverNode.name} (${serverNode.ip})`)
-  let peerConnection = new PeerConnection(serverNode)
-  peerConnection.sendMessage('hello')
-  window.setTimeout(function () {
-    peerConnection.disconnect()
-  }, 4000)
-})
+fileWatcher.startFileWatcher()
 
+fileServer.startFileServer()
+
+// Stop running services before closing window
 window.onbeforeunload = function (event) {
   fileServer.stopFileServer()
   fileWatcher.stopFileWatcher()
   peerDiscovery.stopDiscoverer()
 }
-
-fileWatcher.startFileWatcher()
-
-fileServer.startFileServer()
