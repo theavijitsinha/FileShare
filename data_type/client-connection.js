07d134fcfb3b371bcc22f6a26ee7cd0b4ffab51d
Avijit Sinha <theavijitsinha@gmail.com>
2017-07-29 12:00:24 +0530

    Add ClientConnection class
    Class for handling clients connected to the file server

---
+++ data_type/client-connection.js
@@ -0,0 +1,29 @@
+class ClientConnection {
+  constructor (socket) {
+    this.socket = socket
+    this.buffer = ''
+    this.address = socket.address()
+    console.log(`Client Connected ${this.address.address}`)
+    this.socket.on('end', () => {
+      console.log(`Client Disconnected ${this.address.address}`)
+    })
+    this.socket.on('data', this.dataHandler.bind(this))
+  }
+
+  dataHandler (data) {
+    this.buffer = this.buffer + data
+    let pattern = /^([^\r\n]+)[\r\n]+([\s\S]*)$/
+    let bufferMatch = this.buffer.match(pattern)
+    if (bufferMatch !== null) {
+      let message = bufferMatch[1]
+      this.buffer = bufferMatch[2]
+      this.messageHandler(message)
+    }
+  }
+
+  messageHandler (message) {
+    console.log(`File Server Received message from ${this.address.address} : ${message}`)
+  }
+}
+
+module.exports = ClientConnection
--- service/file-server.js
+++ service/file-server.js
@@ -6,13 +6,15 @@ module.exports = {}
 const eventHandler = require('./event-handler.js')
 const Constants = require('./constants.js')
 
-const EventEmitter = require('events')
+const ClientConnection = require('../data_type/client-connection.js')
+
 const net = require('net')
 
 let socketIP = Constants.FILE_SERVER_IP
 let socketPort = Constants.FILE_SERVER_PORT
 
 let server = null
+let clientConnections = {}
 
 module.exports.startFileServer = function () {
   server = net.createServer()
@@ -38,28 +40,5 @@ module.exports.stopFileServer = function () {
 }
 
 function connectionHandler (socket) {
-  let buffer = ''
-  let address = socket.address()
-  console.log(`Client Connected ${address.address}`)
-  socket.on('end', () => {
-    console.log(`Client Disconnected ${address.address}`)
-  })
-  let messageEventHandler = new EventEmitter()
-  socket.on('data', dataHandler)
-  messageEventHandler.on(Constants.EVENT_FILE_SERVER_MESSAGE, messageHandler)
-
-  function dataHandler (data) {
-    buffer = buffer + data
-    let pattern = /^([^\r\n]+)[\r\n]+([\s\S]*)$/
-    let bufferMatch = buffer.match(pattern)
-    if (bufferMatch !== null) {
-      let message = bufferMatch[1]
-      buffer = bufferMatch[2]
-      messageEventHandler.emit(Constants.EVENT_FILE_SERVER_MESSAGE, message)
-    }
-  }
-
-  function messageHandler (message) {
-    console.log(`File Server Received Message : ${message}`)
-  }
+  clientConnections[socket.address().address] = new ClientConnection(socket)
 }
