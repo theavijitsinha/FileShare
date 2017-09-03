0f5701b886fce2e1aba569e5a7c00e1088de3b2f
Avijit Sinha <theavijitsinha@gmail.com>
2017-09-03 20:22:08 +0530

    Fix messageHandler not called for multiple messages
    messageHandler was only called for first message when multiple messages are present in buffer

--- data_type/connection.js
+++ data_type/connection.js
@@ -2,17 +2,18 @@ class Connection {
   constructor (socket) {
     this.buffer = ''
     this.socket = socket
-    this.socket.on('data', dataHandler.bind(this))
+    this.socket.on('data', this.dataHandler.bind(this))
+  }
 
-    function dataHandler (data) {
-      this.buffer = this.buffer + data
-      let pattern = /^([^\r\n]+)[\r\n]+([\s\S]*)$/
-      let bufferMatch = this.buffer.match(pattern)
-      if (bufferMatch !== null) {
-        let message = bufferMatch[1]
-        this.buffer = bufferMatch[2]
-        this.messageHandler(JSON.parse(message))
-      }
+  dataHandler (data) {
+    this.buffer = this.buffer + data
+    let pattern = /^([^\r\n]+)[\r\n]+([\s\S]*)$/
+    let bufferMatch = this.buffer.match(pattern)
+    while (bufferMatch !== null) {
+      let message = bufferMatch[1]
+      this.buffer = bufferMatch[2]
+      this.messageHandler(JSON.parse(message))
+      bufferMatch = this.buffer.match(pattern)
     }
   }
 
